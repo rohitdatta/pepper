7af409428990e651a237a8d3daffdd20af009fe4
Rohit Datta <rohitdatta11@gmail.com>
2016-07-15 08:19:32 -0700

    can log into system as a repeat user

--- nucleus/users/views.py
+++ nucleus/users/views.py
@@ -16,13 +16,14 @@ def landing():
 	return render_template("static_pages/index.html")
 
 def dashboard():
-	return render_template('users/dashboard.html')
+	return render_template('users/dashboard.html', user=current_user)
 
 def callback():
 	url = 'https://my.mlh.io/oauth/token?client_id=61d0d7bf56ab02f4a0dcdddc26816c27164748890de08ddd809877ba3f229b15&client_secret=67a6dcad88e1a881934518f5abc6beba46fc6910756b7f1c2a85f7dafd0bf5a7&code='+ request.args.get('code')+ '&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2Fcallback&grant_type=authorization_code'
 	resp = requests.post(url)
 	# print resp.headers
 	access_token = resp.json()['access_token']
+	print 'ACCESS TOKEN:' + access_token
 	user = User.query.filter_by(email=access_token).first()
 	if user is None:
 		# create the user
@@ -38,8 +39,8 @@ def callback():
 			flash('A fatal error occurred. Please contact us for help', 'error')
 			return render_template('static_pages/index.html')
 	else:
-		login_user(user.first(), remember=True)
-		return render_template('users/dashboard.html')
+		login_user(user, remember=True)
+		return redirect(url_for('dashboard'))
 	return redirect(url_for('confirm-registration'))
 	# return 'Access token for current user: ' + access_token
 
