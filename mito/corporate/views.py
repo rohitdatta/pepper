2cd7c34c526ce034d53b444f493ee73925531b90
Rohit Datta <rohitdatta11@gmail.com>
2016-07-28 23:54:57 -0700

    wrote a basic search for users

--- mito/corporate/views.py
+++ mito/corporate/views.py
@@ -3,6 +3,7 @@ from flask.ext.login import login_user, current_user, login_required
 from mito.users import User
 from helpers import check_password
 from mito.utils import corp_login_required, roles_required
+from sqlalchemy import or_
 
 def login():
 	if request.method == 'GET':
@@ -29,4 +30,24 @@ def corporate_dash():
 @corp_login_required
 @roles_required(['corp', 'admin'])
 def corporate_search():
-	return 'search'
+	universities = request.args.get('u')
+	if universities:
+		universities = [university.strip() for university in universities.split(',')]
+	class_standings = request.args.get('c')
+	if class_standings:
+		class_standings = [class_standing.strip() for class_standing in class_standings.split(',')]
+	majors = request.args.get('m')
+	if majors:
+		majors = [major.strip() for major in majors.split(',')]
+	users = User.query
+	if universities:
+		users = users.filter(User.school.in_(universities))
+		all_users = users.all()
+	if majors:
+		users = users.filter(User.major.in_(majors))
+		all_users = users.all()
+	if class_standings:
+		users = users.filter(User.class_standing.in_(class_standings))
+		all_users = users.all()
+	users = users.all()
+	return render_template('corporate/results.html', universities=universities, class_standings=class_standings, majors=majors)
