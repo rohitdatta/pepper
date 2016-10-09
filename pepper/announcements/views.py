25e5eed3a79ff6b536c7fcc1fd29abbc2112fcc7
Rohit Datta <rohitdatta11@gmail.com>
2016-10-09 11:17:23 -0500

    send notifications to firebase

--- pepper/announcements/views.py
+++ pepper/announcements/views.py
@@ -3,6 +3,7 @@ from flask import jsonify, request
 from models import Announcement
 from pepper import settings
 from datetime import datetime
+import requests
 
 def announcement_list():
 	announcements = Announcement.query().all()
@@ -14,12 +15,25 @@ def create_announcement():
 	ts = datetime.fromtimestamp(float(request.form.get('timestamp')))
 	if request.form.get('token') != settings.SLACK_TOKEN:
 		return 'Unauthorized', 401
-	send_notification = text.startswith('@channel')
+	send_notification = text.startswith('<!channel>')
 	if send_notification:
-		text = text[8:]
+		text = text[10:]
 	announcement = Announcement(text, ts)
 	DB.session.add(announcement)
 	DB.session.commit()
 
+	if send_notification:
+		resp = requests.post('https://fcm.googleapis.com/fcm/send', headers={
+			'Authorization': 'key={}'.format(settings.FIREBASE_KEY)
+		}, data={
+			'to': "/topics/announcement",
+			'time_to_live': 0,
+			'data': {
+				'title': 'HackTX',
+				'text': text,
+				'vibrate': True
+			}
+		})
+		print resp.status_code
 	# Create a POST to Firebase
 	return 'Created announcement'
