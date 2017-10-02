66c16d807f8ebcba0cf56a5875c6a687580ab033
Eric Lee <hownl96@gmail.com>
2017-10-02 15:38:30 -0500

    Added remove user ability for team leaders

--- pepper/teams/views.py
+++ pepper/teams/views.py
@@ -1,6 +1,6 @@
 from flask.ext.login import login_required, current_user
 from flask import request, render_template, redirect, url_for
-from helpers import join_team, create_team, rename_team, leave_team
+from helpers import join_team, create_team, rename_team, leave_team, remove_team
 
 @login_required
 def team():
@@ -20,5 +20,7 @@ def team():
             return rename_team(request)
         elif val == 'leave':
             return leave_team(request)
+        elif val == 'remove':
+            return remove_team(request)
         else:
             return redirect(url_for('team'))
