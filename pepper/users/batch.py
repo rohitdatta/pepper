8f998238a34c6761794b5331c64b9b4aea490d6b
Raymond Chee <rjchee@gmail.com>
2017-10-02 10:02:01 -0500

    whoops

--- pepper/users/batch.py
+++ pepper/users/batch.py
@@ -3,7 +3,7 @@ from pepper import settings
 from pepper.app import DB
 from pepper.utils import send_email, serializer, timed_serializer
 
-from flask import render_template, render_template_string, url_for
+from flask import g, render_template, render_template_string, url_for
 from sqlalchemy import or_
 
 import random
