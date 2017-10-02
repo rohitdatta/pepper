de12e085f8b1983db01b2282817f0822ae9f7db0
Raymond Chee <rjchee@gmail.com>
2017-10-01 20:07:43 -0500

    Add creation time to team db model

--- pepper/teams/models.py
+++ pepper/teams/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pepper.app import DB
 
 
@@ -7,7 +9,9 @@ class Team(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     tname = DB.Column(DB.String(255), unique=True)
     users = DB.relationship("User", back_populates="team")
+    time_created = DB.Column(DB.DateTime)
 
     def __init__(self, name, creator):
         self.tname = name
         self.users = [creator]
+        self.time_created = datetime.utcnow()
