b00507cf4cd8092beccd4489d3add85fd61de1f7
Raymond Chee <rjchee@gmail.com>
2017-10-06 16:23:35 -0500

    Add easier context to management shell

--- manage.py
+++ manage.py
@@ -31,5 +31,12 @@ def run(port=5000):
     hackathon_identity_app.run(port=int(port))
 
 
+@manager.shell
+def make_shell_context():
+    from pepper.teams.models import Team
+    from pepper.users.models import User, UserRole
+    return dict(app=hackathon_identity_app, DB=app.DB, Team=Team, User=User, UserRole=UserRole)
+
+
 if __name__ == "__main__":
     manager.run()
