07e0b206b7bb0d8190efdbd1c5b06fabe26ba3c9
Arjun Kunjilwar <arjunkunjilwar@utexas.edu>
2018-09-22 22:24:43 -0500

    Added script

---
+++ scripts/accept_user_by_id.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from flask_script import Command, Option
+from pepper.utils import serializer
+
+from pepper.users.models import User
+from pepper.app import DB
+from pepper.utils import send_email
+from pepper import status, settings
+
+class AcceptUserByID(Command):
+
+    option_list = (
+        Option('--user_id', dest='user_id'),
+    )
+
+    def run(self, user_id):
+        user = User.query.filter(User.id==user_id).first()
+        if user is None:
+            print 'There is no user with user_id={}'.format(user.id)
+        else:
+            user.status = status.ACCEPTED
+            html = render_template('emails/application_decisions/accepted.html', user=user)
+            DB.session.add(user)
+            DB.session.commit()
+            # send_email(settings.GENERAL_INFO_EMAIL, "Your {} Application Status".format(settings.HACKATHON_NAME),
+                    #    user.email, html_content=html)
+            print 'Accepting user_id={}'.format(user.id)
+
