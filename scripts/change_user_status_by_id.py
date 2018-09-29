6d331e18297a452796fc0a8fb76bfb62695b25ca
Arjun Kunjilwar <arjunkunjilwar@utexas.edu>
2018-09-28 19:07:49 -0500

    now can accept, waitlist, reject users by id

---
+++ scripts/change_user_status_by_id.py
@@ -0,0 +1,46 @@
+from flask import render_template
+from flask_script import Command, Option
+from pepper.utils import serializer
+
+from pepper.users.models import User
+from pepper.app import DB
+from pepper.utils import send_email
+from pepper import status, settings
+
+class AcceptUserByID(Command):
+
+    option_list = (
+        Option('--new_status', dest='new_status'),
+        Option('--user_id', dest='user_id'),
+        
+    )
+
+    def run(self, user_id, new_status):
+        user = User.query.filter(User.id==user_id).first()
+        if user is None:
+            print 'There is no user with user_id={}'.format(user_id)
+        elif new_status is None:
+            print 'Please provide a user status'
+        else:
+            new_status = new_status.lower()
+            changed_status = False
+            if (new_status == 'accepted'):
+                user.status = status.ACCEPTED
+                html = render_template('emails/application_decisions/accepted.html', user=user)
+                changed_status = True
+            elif (new_status == 'waitlisted'):
+                user.status = status.WAITLISTED
+                html = render_template('emails/application_decisions/waitlisted.html', user=user)
+                changed_status = True
+            elif (new_status == 'rejected'):
+                user.status = status.REJECTED
+                html = render_template('emails/application_decisions/rejected.html', user=user)
+                changed_status = True
+            else:
+                print '{} is not a valid status'.format(new_status)
+            if (changed_status):            
+                DB.session.add(user)
+                DB.session.commit()
+                send_email(settings.GENERAL_INFO_EMAIL, "Your {} Application Status".format(settings.EVENT_NAME), user.email, html_content=html)
+                print '{} user_id={}'.format(new_status, user.id)
+
