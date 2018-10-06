82cfb58dbfd2237a1047d338aa7bd102bf6f48d1
Shreyas Sood <shreyassood@gmail.com>
2018-10-06 16:53:27 -0500

    Safer change status script

--- scripts/change_user_status_by_id.py
+++ scripts/change_user_status_by_id.py
@@ -10,39 +10,43 @@ from pepper import status, settings
 class ChangeUserStatusByID(Command):
 
     option_list = (
-        Option('--new_status', dest='new_status'),
-        Option('--user_id', dest='user_id'),
-        
+        Option('--current_status', dest='current_status', required=True),
+        Option('--new_status', dest='new_status', required=True),
+        Option('--user_id', dest='user_id', required=True),
     )
 
-    def run(self, user_id, new_status):
+    def run(self, user_id, new_status, current_status):
         if user_id is None or new_status is None:
-            print "Please specify user_id and new_status"
+            print('Please specify user_id and new_status')
             return
 
         user = User.query.filter(User.id==user_id).first()
         if user is None:
-            print 'There is no user with user_id={}'.format(user_id)
+            print('There is no user with user_id={}'.format(user_id))
+            return
+
+        if current_status is not None:
+            assert user.status.lower() == current_status.lower(), 'User status is {}'.format(user.status)
+
+        new_status = new_status.lower()
+        changed_status = False
+        if (new_status == 'accepted'):
+            user.status = status.ACCEPTED
+            html = render_template('emails/application_decisions/accepted.html', user=user)
+            changed_status = True
+        elif (new_status == 'waitlisted'):
+            user.status = status.WAITLISTED
+            html = render_template('emails/application_decisions/waitlisted.html', user=user)
+            changed_status = True
+        elif (new_status == 'rejected'):
+            user.status = status.REJECTED
+            html = render_template('emails/application_decisions/rejected.html', user=user)
+            changed_status = True
         else:
-            new_status = new_status.lower()
-            changed_status = False
-            if (new_status == 'accepted'):
-                user.status = status.ACCEPTED
-                html = render_template('emails/application_decisions/accepted.html', user=user)
-                changed_status = True
-            elif (new_status == 'waitlisted'):
-                user.status = status.WAITLISTED
-                html = render_template('emails/application_decisions/waitlisted.html', user=user)
-                changed_status = True
-            elif (new_status == 'rejected'):
-                user.status = status.REJECTED
-                html = render_template('emails/application_decisions/rejected.html', user=user)
-                changed_status = True
-            else:
-                print '{} is not a valid status'.format(new_status)
-            if (changed_status):            
-                DB.session.add(user)
-                DB.session.commit()
-                send_email(settings.GENERAL_INFO_EMAIL, "Your {} Application Status".format(settings.EVENT_NAME), user.email, html_content=html)
-                print '{} user_id={}'.format(new_status, user.id)
+            print('{} is not a valid status'.format(new_status))
+        if (changed_status):
+            DB.session.add(user)
+            DB.session.commit()
+            send_email(settings.GENERAL_INFO_EMAIL, 'Your {} Application Status'.format(settings.EVENT_NAME), user.email, html_content=html)
+            print('{} user_id={}'.format(new_status, user.id))
 
